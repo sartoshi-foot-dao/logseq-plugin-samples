bd71af5a3ffc8c866bc2f383c7d68293927078db
Ziding Zhang <zidingz@gmail.com>
2022-05-14 16:17:05 +0100

    achieve tings

--- logseq-hello-world/index.js
+++ logseq-hello-world/index.js
@@ -1,8 +1,20 @@
 /**
  * entry
  */
-function main () {
-  logseq.App.showMsg('❤️ Message from Hello World Plugin :)')
+
+
+
+function main() {
+  const endpoint = 'https://europe-west2-hypothesis-roam.cloudfunctions.net/unsyncedDocuments?collectionId=tweets'
+
+  const data = fetch(endpoint).then(res => res.json()).catch((err) => {
+    console.log(err);
+  });
+  console.log(data);
+
+
+  logseq.App.showMsg('❤️ Message from Hello World Plugin :)');
+  logseq.App.showMsg(data);
 }
 
 // bootstrap
