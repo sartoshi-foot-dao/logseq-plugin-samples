b0cb17953cb3ea2674319d4d7bacf21f918f76a5
Ziding Zhang <zidingz@gmail.com>
2022-05-14 16:16:46 +0100

    achieve page manipulation

--- logseq-reddit-hot-news/index.ts
+++ logseq-reddit-hot-news/index.ts
@@ -3,76 +3,144 @@ import { LSPluginBaseInfo } from '@logseq/libs/dist/libs'
 
 const delay = (t = 100) => new Promise(r => setTimeout(r, t))
 
-async function loadRedditData () {
-  const endpoint = 'https://www.reddit.com/r/logseq/hot.json'
+async function fetchUnsyncedTweets() {
+  const endpoint = 'https://europe-west2-hypothesis-roam.cloudfunctions.net/unsyncedDocuments?collectionId=tweets'
 
-  const { data: { children } } = await fetch(endpoint).then(res => res.json())
-  const ret = children || []
+  const data = await fetch(endpoint).then(res => res.json()).catch((err) => {
+    console.log(err);
+  });
 
-  return ret.map(({ data }, i) => {
-    const { title, selftext, url, ups, downs, num_comments } = data
-
-    return `${i}. [${title}](${url}) [:small.opacity-50 "🔥 ${ups} 💬 ${num_comments}"]
-collapsed:: true    
-> ${selftext}`
-  })
+  return data
 }
 
+async function fetchUnsyncedAnnotations() {
+  const endpoint = 'https://europe-west2-hypothesis-roam.cloudfunctions.net/unsyncedDocuments?collectionId=annotations'
+
+  const data = await fetch(endpoint).then(res => res.json()).catch((err) => {
+    console.log(err);
+  });
+
+  return data
+}
+
+const createPage = async (pageName) => {
+  logseq.App.pushState('page', { name: pageName });
+  await delay(300);
+  return logseq.Editor.getCurrentPage();
+}
+
+const createPages = async (pageNames: Array<string>) => {
+  for (const pageName of pageNames) {
+    console.log(await createPage(pageName));
+  }
+}
+
+const getPage = async (name) => {
+  return await logseq.Editor.getPage(name);
+}
+
+const deleteBlock = async (uuid) => {
+  await logseq.Editor.removeBlock(uuid);
+}
+
+const deleteBlocks = async (pageNames: Array<string>) => {
+  await Promise.all(pageNames.map(async (pageName) => {
+    const { uuid } = await getPage(pageName)
+    console.log(`deleting ${pageName} uuid: ${uuid}`);
+    return deleteBlock(uuid);
+  }))
+}
+
+// const createPage = async (name) => {
+//   return await logseq.Editor.
+// };
 /**
  * main entry
  * @param baseInfo
  */
-function main (baseInfo: LSPluginBaseInfo) {
-  let loading = false
+async function main(baseInfo: LSPluginBaseInfo) {
+  const testPages = ['toosting1', 'toosting2', 'toosting3', 'toosting4', 'toosting5'];
 
   logseq.provideModel({
-    async loadReddits () {
+    async deleteTestPages() {
+      await deleteBlocks(testPages);
+    },
+    async createTestPages() {
 
-      const info = await logseq.App.getUserConfigs()
-      if (loading) return
 
-      const pageName = 'reddit-logseq-hots-news'
-      const blockTitle = (new Date()).toLocaleString()
+      // console.log(await fetchUnsyncedTweets());
+      // console.log(await fetchUnsyncedAnnotations());
 
-      logseq.App.pushState('page', { name: pageName })
+      // const info = await logseq.App.getUserConfigs()
+      // if (loading) return
 
-      await delay(300)
+      // const pageName = 'reddit-logseq-hots-news'
+      // const blockTitle = (new Date()).toLocaleString()
 
-      loading = true
+      
 
-      try {
-        const currentPage = await logseq.Editor.getCurrentPage()
-        if (currentPage?.originalName !== pageName) throw new Error('page error')
+      const createdPages = await createPages(testPages);
 
-        const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree()
-        let targetBlock = pageBlocksTree[0]!
+      // await deleteBlocks(pages);
 
-        targetBlock = await logseq.Editor.insertBlock(targetBlock.uuid, '🚀 Fetching r/logseq ...', { before: true })
+      // const page = await getPage('toosting3');
+      // console.log({ page });
+      // await deleteBlock(page.uuid);
 
-        let blocks = await loadRedditData()
+      // for (const page of pages) {
+      //   gotoOrCreatePage(page);
+      //   await delay(300);
+      // }
+      // const toosting = await Promise.all(pages.map((page) => {
+      //   return gotoOrCreatePage(page);
+      // }));
 
-        blocks = blocks.map(it => ({ content: it }))
+      // console.log(toosting);
 
-        await logseq.Editor.insertBatchBlock(targetBlock.uuid, blocks, {
-          sibling: false
-        })
+      
 
-        await logseq.Editor.updateBlock(targetBlock.uuid, `## 🔖 r/logseq - ${blockTitle}`)
-      } catch (e) {
-        logseq.App.showMsg(e.toString(), 'warning')
-        console.error(e)
-      } finally {
-        loading = false
-      }
+
+
+
+      // loading = true
+
+      // try {
+      //   const currentPage = await logseq.Editor.getCurrentPage()
+      //   if (currentPage?.originalName !== pageName) throw new Error('page error')
+
+      //   const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree()
+      //   let targetBlock = pageBlocksTree[0]!
+
+      //   targetBlock = await logseq.Editor.insertBlock(targetBlock.uuid, '🚀 Fetching r/logseq ...', { before: true })
+
+      // let blocks = await loadRedditData()
+
+      //   blocks = blocks.map(it => ({ content: it }))
+
+      //   await logseq.Editor.insertBatchBlock(targetBlock.uuid, blocks, {
+      //     sibling: false
+      //   })
+
+      //   await logseq.Editor.updateBlock(targetBlock.uuid, `## 🔖 r/logseq - ${blockTitle}`)
+      // } catch (e) {
+      //   logseq.App.showMsg(e.toString(), 'warning')
+      //   console.error(e)
+      // } finally {
+      //   loading = false
+      // }
     }
   })
 
   logseq.App.registerUIItem('toolbar', {
     key: 'logseq-reddit',
     template: `
-      <a data-on-click="loadReddits"
+      <a style="display: inline-block" data-on-click="createTestPages"
          class="button">
-        <i class="ti ti-brand-reddit"></i>
+        <span>create test pages</span>
+      </a>
+      <a style="display: inline-block" data-on-click="deleteTestPages"
+         class="button">
+        <span>delete test pages</span>
       </a>
     `
   })
